197a32dc36e8f2bed86c6214cd9b5016c737edf1
Yatii2 <37732@ma-web.nl>
2024-09-27 11:50:27 +0200

    4 en 5

--- Assets/Scripts/3/EnemySpawner.cs
+++ Assets/Scripts/3/EnemySpawner.cs
@@ -4,21 +4,52 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    public GameObject enemy;
        
-    private List<GameObject> enemies = new List<GameObject>(); 
+    private List<GameObject> enemies = new List<GameObject>();
+    private float spawnRate = 1f;
+    private float nextSpawnTime = 0f;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            
+            for (int i = 0; i < 100; i++)
+            {
+                SpawnEnemy();
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            
+            ClearEnemies();
         }
+
+        if (Time.time >= nextSpawnTime)
+        {
+            nextSpawnTime = Time.time + spawnRate;
+            for (int i = 0; i < 3; i++)
+            {
+                SpawnEnemy();
+            }
+        }
+
+    }
+    private void SpawnEnemy()
+    {
+        GameObject newEnemy = Instantiate(enemy);
+        enemies.Add(newEnemy); 
     }
 
-    
+
+    private void ClearEnemies()
+    {
+        foreach (GameObject enemy in enemies)
+        {
+            Destroy(enemy);
+        }
+        enemies.Clear();
+    }
 }
+
+
